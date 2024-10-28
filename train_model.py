2969be96a229384eccb99db86690351fb1c8f6a9
Bernard Ginn Jr. <117537210+BG-legacy@users.noreply.github.com>
2024-10-28 17:15:32 -0400

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,4 +3,6 @@ from Model import SkinCancerModel
 if __name__ == "__main__":
     metadata_path = './HAM10000_metadata.csv'
     images_dir = './HAM10000_images_part_1'
-    SkinCancerModel.train_with_ham10000(metadata_path, images_dir)
+    model = SkinCancerModel()
+    model.train_with_ham10000(metadata_path, images_dir)
+    print("Training complete!")
